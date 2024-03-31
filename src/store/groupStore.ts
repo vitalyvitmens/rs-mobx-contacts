7f5bfbde92296292aa449e99c5c46a2289284985
vitalyvitmens <otm_@tut.by>
2024-03-31 23:15:35 +0300

    feat(src\store\groupStore.ts): создаёт groupStore

---
+++ src/store/groupStore.ts
@@ -0,0 +1,14 @@
+import { makeAutoObservable } from 'mobx'
+import { api } from '../api'
+import { GroupContactsDto } from 'src/types/dto/GroupContactsDto'
+
+export const groupStore = makeAutoObservable({
+  groupContacts: [] as GroupContactsDto[],
+  *get() {
+    const result: GroupContactsDto[] = yield api.getGroupContacts()
+
+    if (result) {
+      this.groupContacts = result
+    }
+  },
+})
